6ef326ed683e2ba71128af255d121ac510c7efa2
ebimammadi <ebi@tadbir.net>
2020-06-05 16:50:01 +0200

    costom config header middleware

---
+++ middleware/responseHeader.js
@@ -0,0 +1,9 @@
+const configResponseHeader = function(req, res, next) {
+	//res.header("Access-Control-Allow-Origin", "*");
+	//res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+	res.header("Access-Control-Expose-Headers", "x-auth-token");
+	//res.header("Access-Control-Allow-Headers", "x-auth-token");
+	next();
+}
+
+module.exports = configResponseHeader;
