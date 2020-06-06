78b8c33790af914d82e586457604b3d2c26c41fd
ebimammadi <ebi@tadbir.net>
2020-06-06 13:20:37 +0200

    refactored

--- index.js
+++ index.js
@@ -15,23 +15,14 @@ const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const usersRoute = require('./routes/users');
 
-
-if (!process.env.JWT_KEY) {
-	console.error('Fatal Error: jwtPrivateKey is not defined.');
-	process.exit(1);
-}
-
 //mongoose.connect('mongodb://localhost/playground')
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then( () => console.log('mongoDB connected ...'))
-	.catch( (err) => console.log('Error connecting MongoDB... ',err));
+	.catch( (err) => console.error('Error connecting MongoDB... ',err));
 mongoose.set('useCreateIndex', true);
 
 //Middleware 
-//adds 2 seconds of intentional latency
-//app.use( (req,res, next) => setTimeout(next, 500))
-
-
+app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 seconds of intentional latency
 app.use(configResponseHeader);
 app.use(express.json());
 app.use(bodyParser.json());
@@ -43,7 +34,7 @@ app.use('/', homeRoutes);
 app.use('/validate', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/users', usersRoute);
-//app.get('/auth', authRoute);
 
+//Setting the port for the application
 const port = process.env.PORT || 8080;
 app.listen(port,'localhost',() => console.log(`Listing on port ${port}...`) );
--- middleware/responseHeader.js
+++ middleware/responseHeader.js
@@ -1,4 +1,4 @@
-const configResponseHeader = function(req, res, next) {
+const responseHeaderConfig = function (req, res, next) {
 	//res.header("Access-Control-Allow-Origin", "*");
 	//res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 	res.header("Access-Control-Expose-Headers", "x-auth-token");
@@ -6,4 +6,4 @@ const configResponseHeader = function(req, res, next) {
 	next();
 }
 
-module.exports = configResponseHeader;
+module.exports = responseHeaderConfig;
