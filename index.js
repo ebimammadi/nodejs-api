1d86788194c231189f78adade62e44447e6b2552
ebimammadi <ebi@tadbir.net>
2020-06-25 18:37:42 +0200

    refactored for upload sent to files route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const config = require('dotenv/config'); //to read .env file
 const express = require('express');
 const app = express();
+const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -16,7 +17,7 @@ const { responseHeaderConfig } = require('./middleware/headersCookie.js');
 const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const usersRoute = require('./routes/users');
-
+const fileRoutes = require('./routes/files');
 //mongoose.connect('mongodb://localhost/playground')
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then( () => console.log('mongoDB connected ...'))
@@ -24,26 +25,28 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopo
 mongoose.set('useCreateIndex', true);
 
 //Middleware 
-app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 seconds of intentional latency
-//app.use( helmet()); //! for security, it should be reactivated
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-app.use(responseHeaderConfig);
-app.use(express.json());
+app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 second of latency intentionally
+app.use( helmet()); //! for security, it should be checked fully
+app.use(cors({ credentials: true, origin: process.env.APP_PATH }));
+app.use(responseHeaderConfig);//configures the header for requests
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb',extended: true }));
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true })); // key=value&key=value
+//app.use(bodyParser.json({ limit: '50mb' }));//!important temporarily
+//app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
+//app.use(express.urlencoded({ extended: true })); // key=value&key=value
+
 
 //!BLOCK suspecious requests to the server by IP
 
 //!Routes
+
 app.use('/', homeRoutes);
 app.use('/about', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/users', usersRoute);
-
+app.use('/files', fileRoutes);
 //Setting the port for the application
 const port = process.env.PORT || 8080;
 console.log('---------------------------------------------------------------')
 app.listen(port,'localhost',() => console.log(`Listing on port ${port}...`) );
-//https.createServer(app).listen(port,'localhost',() => console.log(`Listing on port ${port}...`));
-//httpsServer.listen(port,'localhost',() => console.log(`Listing on port ${port}...`));
