32cf37f6e1eb9af5bb5f32c03fde0dec6057314f
ebimammadi <ebi@tadbir.net>
2020-06-26 15:31:27 +0200

    upload limit set to 16mb

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const config = require('dotenv/config'); //to read .env file
 const express = require('express');
 const app = express();
-//const path = require('path');
 const helmet = require('helmet');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -11,8 +10,6 @@ const mongoose = require('mongoose');
 
 //Setting the port for the application
 const port = process.env.PORT || 8080;
-// const Joi = require('joi');//!depricated, instead hapi/joi
-// Joi.onjectId = require('joi-objectid')(Joi);
 
 //customized_modules
 const { responseHeaderConfig } = require('./middleware/headersCookie.js');
@@ -21,6 +18,7 @@ const homeRoutes = require('./routes/home');
 const coursesRoutes = require('./routes/courses');
 const usersRoute = require('./routes/users');
 const fileRoutes = require('./routes/files');
+
 //mongoose.connect('mongodb://localhost/playground')
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then( () => console.log('mongoDB connected ...'))
@@ -28,27 +26,24 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopo
 mongoose.set('useCreateIndex', true);
 
 //Middleware 
-app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 second of latency intentionally
-app.use( helmet()); //! for security, it should be checked fully
+//app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 second of latency intentionally
+app.use(helmet()); //! for security, it should be checked fully
 app.use(cors({ credentials: true, origin: process.env.APP_PATH }));
 app.use(responseHeaderConfig);//configures the header for requests
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb',extended: true }));
+app.use(express.json({ limit: '16mb' }));
+app.use(express.urlencoded({ limit: '16mb',extended: true }));
 app.use(cookieParser());
 //app.use(bodyParser.json({ limit: '50mb' }));//!important temporarily
 //app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
 //app.use(express.urlencoded({ extended: true })); // key=value&key=value
 
-
 //!BLOCK suspecious requests to the server by IP
 
-//!Routes
-
+//Routes
 app.use('/', homeRoutes);
 app.use('/about', homeRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/users', usersRoute);
 app.use('/files', fileRoutes);
 
-console.log('---------------------------------------------------------------')
 app.listen(port,'localhost',() => console.log(`Listing on port ${port}...`) );
