f6633f66e7af2a75a6153626ff28512d63be38a7
ebimammadi <ebi@tadbir.net>
2020-06-28 20:28:14 +0200

    delete-image applied

--- routes/files.js
+++ routes/files.js
@@ -6,21 +6,22 @@ const Joi = require('@hapi/joi');
 const mime = require('mime');
 const base64Img = require('base64-img');
 const jwt = require ('jsonwebtoken');
-const sha256 = require('js-sha256');
-//const _ = require('lodash'); 
 
 const { User } = require('../models/user');
 const auth = require('../middleware/auth');
 
+//!file protections should be developed!!!
+
+
 router.post('/upload-image', auth, async(req, res) => {
 	const { error } = validateImage(req.body);
 	if (error) return res.json({ response_type: 'error', message: `${error.details[0].message}` });
-	//generate-folder
 	try{
-		const token = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY);
-		const folder = generateFolderName(req.body.usage, token);
+		//generate-folder
+		const { _id } = jwt.verify( req.cookies["x-auth-token"], process.env.JWT_KEY);
+		const folder = generateEncodedFolderName(req.body.usage, _id);
 		//read record from database
-		let user = await User.findOne({ _id: token._id });
+		let user = await User.findOne({ _id });
 		let profilePhotoUrl = user.profilePhotoUrl; 
 		//remove previous file
 		if (req.body.unique === 'true' && profilePhotoUrl ) await removeFromStorage(profilePhotoUrl);
@@ -35,16 +36,38 @@ router.post('/upload-image', auth, async(req, res) => {
 			url: `${process.env.API_PATH}/files${uploadPath}`
 		});
 	}catch(err){
-		return res.json({ response_type: 'error', message: `error!` });
+		return res.json({ response_type: 'warning', message: err.message });
+	}
+});
+
+router.get('/delete-image/:folder/:file', auth, async(req, res) => {
+	//! 0.check credential
+	try{
+		//1. check folder validity & extract _id 
+		const { usage, _id } = generateDecodedFolderName(req.params.folder);
+		if (!validPrefixes.includes(usage)) return res.json({ response_type: 'danger', message: 'invalid path' });
+		let user = await User.findOne({ _id });
+		const profilePhotoUrl = '/' + req.params.folder + '/' + req.params.file;
+			
+		if (!user || (user.profilePhotoUrl != profilePhotoUrl) ) return res.json({ response_type: 'danger', message: 'invalid path' });
+		//2. check if file exists
+		const pathFile = absolutePath( profilePhotoUrl ) ;
+		//3. delete file from server
+		if (fs.existsSync(pathFile)) fs.unlinkSync(pathFile);
+		//4. update filename
+		user.set({ profilePhotoUrl: '' });
+		await user.save();	
+		//5. return
+		return res.json({ response_type: 'success', url: profilePhotoUrl, message: 'removed successfully.' });
+	}catch(err){
+		return res.json({ response_type: 'danger', message: err.message });
 	}
 
 });
 
-//!file protections should be developed!!!
-//this part is for protected files 
-router.get('/p/:folderName/:fileName', auth, (req, res) => {
-	const filename = path.dirname(require.main.filename) + '/' + process.env.UPLOAD_FOLDER
-								+ '/p/' + req.params.folderName + '/' + req.params.fileName;
+//!protected files 
+router.get('/p/:folder/:file', auth, (req, res) => {
+	const filename = absolutePath( '/p/' + req.params.folder + '/' + req.params.file);
 	//!more authentication for protected files (own-user and admin only)
 	try {   
 		if (fs.existsSync(filename)) {     
@@ -57,9 +80,9 @@ router.get('/p/:folderName/:fileName', auth, (req, res) => {
 	}
 });
 
-router.get('/:folderName/:fileName' , (req, res) => {
-	const filename = path.dirname(require.main.filename) + '/' + process.env.UPLOAD_FOLDER
-								+ '/' + req.params.folderName + '/' + req.params.fileName;
+//public files
+router.get('/:folder/:file' , (req, res) => {
+	const filename = absolutePath( '/' + req.params.folder + '/' + req.params.file);
 	try {   
 		if (fs.existsSync(filename)) {     
 			res.writeHead(200, {'Content-Type': mime.getType(filename)});      
@@ -97,10 +120,14 @@ const uploadToStorage = (image, usage, folder) => {
 	})
 };
 
-const generateFolderName = (usage, token) => {
-	//usage `profile`
-	return `${usage}-${sha256(token._id)}`;
+const generateEncodedFolderName = (usage, _id) => {
+	return Buffer.from(usage+'-'+_id).toString('hex') //!encode base64
+	//revernse Buffer.from(hexEncoded, 'base64').toString()
 };
+const generateDecodedFolderName = (foldername) => {
+	const [ usage, _id ] = Buffer.from(foldername, 'hex').toString().split('-');
+	return { usage, _id };
+}
 
 const validateImage = (image) => {	
 	const imageSchema = Joi.object({
@@ -110,5 +137,10 @@ const validateImage = (image) => {
 	});
 	return imageSchema.validate(image);
 };
+
+const absolutePath = pathFile => path.dirname(require.main.filename) + '/' + process.env.UPLOAD_FOLDER + pathFile;
+
+
+const validPrefixes = ['profile','product','point','reciept'];
 
 module.exports = router;
