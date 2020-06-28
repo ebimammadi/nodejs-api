863f36e3239ba8ac860dc5db64ea4e2844ac2728
ebimammadi <ebi@tadbir.net>
2020-06-28 15:37:18 +0200

    code refactor

--- index.js
+++ index.js
@@ -2,14 +2,12 @@
 const config = require('dotenv/config'); //to read .env file
 const express = require('express');
 const app = express();
-const helmet = require('helmet');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-//const bodyParser = require('body-parser'); //!it has been replaced with express.urlencoded!
 const mongoose = require('mongoose');
+const cors = require('cors');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
 
-//Setting the port for the application
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; //!application port
 
 //customized_modules
 const { responseHeaderConfig } = require('./middleware/headersCookie.js');
@@ -19,7 +17,6 @@ const coursesRoutes = require('./routes/courses');
 const usersRoute = require('./routes/users');
 const fileRoutes = require('./routes/files');
 
-//mongoose.connect('mongodb://localhost/playground')
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true } )
 	.then( () => console.log('mongoDB connected ...'))
 	.catch( (err) => console.error('Error connecting MongoDB... ',err));
@@ -27,17 +24,14 @@ mongoose.set('useCreateIndex', true);
 
 //Middleware 
 //app.use( (req,res, next) => setTimeout(next, 500)); //adds 0.5 second of latency intentionally
-app.use(helmet()); //! for security, it should be checked fully
+app.use(helmet()); //! for security issues, it should be developed
 app.use(cors({ credentials: true, origin: process.env.APP_PATH }));
-app.use(responseHeaderConfig);//configures the header for requests
 app.use(express.json({ limit: '16mb' }));
 app.use(express.urlencoded({ limit: '16mb',extended: true }));
 app.use(cookieParser());
-//app.use(bodyParser.json({ limit: '50mb' }));//!important temporarily
-//app.use(bodyParser.urlencoded({ limit: '50mb',extended: true }));
-//app.use(express.urlencoded({ extended: true })); // key=value&key=value
+app.use(responseHeaderConfig);//configures the header for requests
 
-//!BLOCK suspecious requests to the server by IP
+//!DO BLOCK suspecious requests to the server by IP
 
 //Routes
 app.use('/', homeRoutes);
