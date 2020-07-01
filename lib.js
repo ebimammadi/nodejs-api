04ce3286af61108125491116bbcf7e23892a9733
ebimammadi <ebi@tadbir.net>
2020-07-01 10:26:00 +0200

    validURL addded

--- lib.js
+++ lib.js
@@ -12,4 +12,14 @@ const factorial = (n) => {
 	return factorial(n - 1);
 };
 
-module.exports = { escapeText, factorial, regex };
+const validURL = str => {
+  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+};
+
+module.exports = { escapeText, factorial, regex, validURL };
