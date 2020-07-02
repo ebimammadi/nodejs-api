c09045941bf39a9929fa32f0d99354d83df805ee
ebimammadi <ebi@tadbir.net>
2020-07-02 20:16:32 +0200

    urlregexpattern added

--- lib.js
+++ lib.js
@@ -15,16 +15,18 @@ const factorial = (n) => {
 };
 
 const validateURL = str => {
-  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  const pattern = urlRegexPattern;
   return !!pattern.test(str);
 };
+
+const urlRegexPattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
 
 const absolutePath = filePath => path.dirname(require.main.filename) + '/' + process.env.UPLOAD_FOLDER + filePath;
 const urlPath = filePath => (filePath) ? process.env.API_PATH +'/files' + filePath : ''; //return '' if the file is empty
 
-module.exports = { escapeText, factorial, regex, validateURL, absolutePath, urlPath };
+module.exports = { escapeText, factorial, regex, validateURL, absolutePath, urlPath, urlRegexPattern };
