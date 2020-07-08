72c6e29c15ffdd9271d57e74e3d97e2230ca5c1d
ebimammadi <ebi@tadbir.net>
2020-07-08 19:10:56 +0200

    refactor

--- components/nodemailer.js
+++ components/nodemailer.js
@@ -2,9 +2,10 @@ const nodemailer = require('nodemailer');
 const htmlToText = require('html-to-text');
 
 const { Email } = require('../models/email');
+const { emailTemplates } = require('../templates/email');
 
 async function mailer(to,subject,message,template) {
-  if (template) message = useEmailTemplate(message, template);
+  if (template) message = emailTemplates(message, template);
   
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
@@ -45,44 +46,6 @@ async function mailer(to,subject,message,template) {
   // Preview only available when sending through an Ethereal account
   // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-}
-
-const logoLiteral = `<span style="color: #6D9EEB; font-size:18px;" >
-chapar<span style="color: #6AA84F">.tech</span>
-</span>`;
-
-const useEmailTemplate = (message, template) => {
-  if (template =='passwordRecoverTemplate' ){
-    return `${logoLiteral}<br><br>
-      Recovery Link:<br>Please use the following link to recover your password <br>
-      <a href="${process.env.APP_PATH}/recover-password/${message}" target="_blank">
-        ${process.env.APP_PATH}/recover-password/${message}
-      </a>`
-      ;
-  }
-  if (template =='userRegisterTemplate' ){
-    return `${logoLiteral}<br><br>
-      Dear ${message.name},<br>Welcome, thank you for registering at ${process.env.APP_PATH}, please verify your email address by click at the following link:<br>
-      <a href="${process.env.APP_PATH}/verify-email/${message.emailVerify}" target="_blank">
-        ${process.env.APP_PATH}/verify-email/${message.emailVerify}
-      </a><br><br>You can also copy and paste the link to your browser:<br>
-      ${process.env.APP_PATH}/verify-email/${message.emailVerify}`
-      ;
-  }
-  if (template =='emailChangeWarningTemplate' ){
-    return `${logoLiteral}<br><br>
-      Dear ${message.name},<br>Your user email has been changed at ${process.env.APP_PATH}, If you did not send this request, please contact our support. Otherwise you can ignore this email.<br>`
-      ;
-  }
-  if (template =='emailChangeVerifyTemplate' ){
-    return `${logoLiteral}<br><br>
-      Dear ${message.name},<br>This email has been set as your new email account at ${process.env.APP_PATH}, please verify your email address by click at the following link:<br>
-      <a href="${process.env.APP_PATH}/verify-email/${message.emailVerify}" target="_blank">
-        ${process.env.APP_PATH}/verify-email/${message.emailVerify}
-      </a><br><br>You can also copy and paste the link to your browser:<br>
-      ${process.env.APP_PATH}/verify-email/${message.emailVerify}`
-      ;
-  }
 }
 
 module.exports = mailer;
